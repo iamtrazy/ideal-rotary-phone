de8531098b7f3f85319384ace97337201a374a8c
iamtrazy <iamtrazy@proton.me>
2024-09-13 02:28:56 +0530

    fix: test workflow

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Test React App Deployment</h1>
+      <h1>Test React App Deployment (Workflow) </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
